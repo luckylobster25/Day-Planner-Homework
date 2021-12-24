379b0933d0fcfcd6fa25fce9dd49c53d6b3406ba
Chue Chang <chuechang1996@outlook.com>
2021-12-24 00:48:34 -0600

    storing textarea input to local storage

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ setInterval(function(){
 var currentDayel = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
 $("#currentDay").text(currentDayel)
 },1000);
+
+var nineam = document.querySelector("#nineam")
+
+function save1(){
+    localStorage.setItem("save1",nineam.value);
+}
